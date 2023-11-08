ae74ef5c70b0a7bc5ad619dd84109af1070a78b5
Pa1-12 <116073752+Pa1-12@users.noreply.github.com>
2023-11-08 11:45:35 +0530

    Update Deals.tsx

--- store-ui/src/components/Deals/Deals.tsx
+++ store-ui/src/components/Deals/Deals.tsx
@@ -38,42 +38,7 @@ const Deals = () => {
         <Paper elevation={3} sx={{ pl: 2, pb: 2 }}>
             <Typography variant="h6" sx={{ p: 1, color: 'text.primary' }}>Deals of the Day</Typography>
             <Grid container spacing={2} >
-                <>
-                    {
-                        deals.slice(0, 5)?.map((deal: any) => (
-                            <Grid item key={deal.dealId}>
-                                <Link component="button"
-                                    onClick={() => {
-                                        navigate('product/' + deal.variantSku)}
-                                        } underline="none">
-                                    <Card sx={{ width: 250, height: 290 }}>
-                                        <Box><img src={deal.thumbnail} height="150" alt={deal.name}></img></Box>
-                                        <CardContent sx={{ height: 50 }}>
-                                            <Grid container >
-                                                <Grid item xs={12}>
-                                                    <Typography color="text.secondary">
-                                                        {deal.shortDescription}
-                                                    </Typography>
-                                                </Grid>
-                                            </Grid>
-                                        </CardContent>
-                                        <CardActions>
-                                            <Grid container>
-                                                <Grid item xs={6} sx={{ p: 1, display: 'flex', justifyContent: 'flex-start' }}>
-                                                    <Typography variant="h6">$ {deal.price}</Typography></Grid>
-                                                <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
-                                                    <Chip icon={<StarIcon />} label={deal.rating} />
-                                                </Grid>
-                                            </Grid>
-                                        </CardActions>
-                                    </Card>
-                                </Link>
-
-                            </Grid>
-
-                        ))
-                    }
-                </>
+                
             </Grid>
         </Paper>
     )
